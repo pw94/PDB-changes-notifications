b053f57297f6448a70ee3ab4997fbca791230b97
pw94 <pawelmwinnicki@gmail.com>
2017-01-26 23:17:18 +0100

    Extracted function to get secondary structure

--- rnapdbee_fetcher.py
+++ rnapdbee_fetcher.py
@@ -3,31 +3,33 @@ from robobrowser import RoboBrowser
 from zipfile import ZipFile
 import os
 
-pdbId = "5SUP"
-url = "http://rnapdbee.cs.put.poznan.pl/"
-session = Session()
-response = session.post(url + "home/fetch", data={"pdbId":pdbId})
-response.raise_for_status()
-browser = RoboBrowser(parser="html.parser", session=session)
-browser.open(url)
-form = browser.get_form(id="analysePdb")
-datasource = browser.select("#datasourcePdb")
-form["content"].value = response.text
-form["source"].value = pdbId.upper() + ".pdb"
-browser.submit_form(form)
-
-form = browser.get_form(id="downloadResults")
-form["isDotBracket"].value = "on"
-browser.submit_form(form)
-
-results_dir = "results"
-if not os.path.exists(results_dir):
-    os.makedirs(results_dir)
-tmp_filename = os.path.join(results_dir, "tmp.zip")
-with open(tmp_filename, "wb") as output:
-    output.write(browser.response.content)
-
-with ZipFile(tmp_filename,"r") as zip_ref:
-    zip_ref.extractall(results_dir)
-
-os.remove(tmp_filename)
+def get_RNA_secondary_structure(pdbId):
+    url = "http://rnapdbee.cs.put.poznan.pl/"
+    session = Session()
+    response = session.post(url + "home/fetch", data={"pdbId":pdbId})
+    response.raise_for_status()
+    browser = RoboBrowser(parser="html.parser", session=session)
+    browser.open(url)
+    form = browser.get_form(id="analysePdb")
+    datasource = browser.select("#datasourcePdb")
+    form["content"].value = response.text
+    form["source"].value = pdbId.upper() + ".pdb"
+    browser.submit_form(form)
+    
+    form = browser.get_form(id="downloadResults")
+    form["isDotBracket"].value = "on"
+    browser.submit_form(form)
+    
+    results_dir = os.path.abspath("results")
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    tmp_filename = os.path.join(results_dir, "tmp.zip")
+    with open(tmp_filename, "wb") as output:
+        output.write(browser.response.content)
+    
+    with ZipFile(tmp_filename,"r") as zip_ref:
+        zip_ref.extractall(results_dir)
+    
+    os.remove(tmp_filename)
+    
+    return os.path.join(results_dir, os.listdir(results_dir)[0])
