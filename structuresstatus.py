a3d1966d681898750f7acffd87094348022f6b01
pw94 <pawelmwinnicki@gmail.com>
2017-01-27 18:46:23 +0100

    Added functions to get matching structures

---
+++ structuresstatus.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_obsoletes_for(structures):
+    pairs = []
+    for structure in structures:
+        obsolete = get_obsolete(structure)
+        if obsolete is not None:
+            pairs.append((obsolete, structure))
+    
+    return pairs
+
+
+def get_obsolete(id):
+    r = requests.get("http://www.rcsb.org/pdb/rest/idStatus", params={ "structureId" : id })
+    soup = BeautifulSoup(r.text, "xml")
+    return soup.find("record").get("replaces")
+
+
+def get_supersedings_for(structures):
+    pairs = []
+    for structure in structures:
+        superseding = get_obsolete(structure)
+        if superseding is not None:
+            pairs.append((structure, superseding))
+    
+    return pairs
+
+
+def get_superseding(id):
+    r = requests.get("http://www.rcsb.org/pdb/rest/idStatus", params={ "structureId" : id })
+    soup = BeautifulSoup(r.text, "xml")
+    return soup.find("record").get("replacedBy")
